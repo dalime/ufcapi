f1d1b55e7f499da96e464d1a14f9efdc992fffa3
duhdanube <Danny.B.Lim@gmail.com>
2016-10-29 23:29:54 -0400

    Set up basic components

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Layout;
