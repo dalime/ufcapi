19cc74d9e691945b19c23ec231a9134143ec176f
duhdanube <Danny.B.Lim@gmail.com>
2016-12-19 23:46:05 -0500

    Added inputs for new fighter and team

--- src/components/Input/NewFighter.js
+++ src/components/Input/NewFighter.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
 
 export default class NewFighter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      weightClass: '',
+      team: '',
+      events: '',
+      nextEvent: ''
+    };
+
+    this.inputChange = this.inputChange.bind(this);
+    this.addNewFighter = this.addNewFighter.bind(this);
+  }
+
+  inputChange(e) {
+    const key = e.target.dataset.statekey;
+    const value = e.target.value;
+
+    this.setState({
+      [key]: value
+    });
+  };
+
+  addNewFighter() {
+    console.log('ADDED NEW');
+  }
+
   render() {
     return(
       <div>
         <h1>New Fighter</h1>
+        <form onSubmit={this.addNewFighter}>
+          <TextField
+            hintText="Fighter Name"
+            floatingLabelText="Fighter Name"
+            data-statekey="name"
+            onChange={this.inputChange}
+          />
+          <br />
+          <TextField
+            hintText="Weight Class"
+            floatingLabelText="Weight Class"
+            data-statekey="weightClass"
+            onChange={this.inputChange}
+          />
+          <br />
+          <TextField
+            hintText="Team"
+            floatingLabelText="Team"
+            data-statekey="team"
+            onChange={this.inputChange}
+          />
+          <br />
+          <TextField
+            hintText="Events"
+            floatingLabelText="Events"
+            data-statekey="events"
+            onChange={this.inputChange}
+          />
+          <TextField
+            hintText="Next Event"
+            floatingLabelText="Next Event"
+            data-statekey="nextEvent"
+            onChange={this.inputChange}
+          />
+          <br />
+          <RaisedButton label="Add" style={styles.button} type="submit"/>
+        </form>
       </div>
     )
   }
--- src/components/Input/NewTeam.js
+++ src/components/Input/NewTeam.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 
 export default class NewTeam extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      city: '',
+      country: ''
+    };
+
+    this.inputChange = this.inputChange.bind(this);
+    this.addNewTeam = this.addNewTeam.bind(this);
+  }
+
+  inputChange(e) {
+    const key = e.target.dataset.statekey;
+    const value = e.target.value;
+
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addNewTeam() {
+    console.log('NEW TEAM');
+  }
+
   render() {
     return (
       <div>
         <h1>New Team</h1>
+        <form onSubmit={this.addNewTeam}>
+          <TextField
+            hintText="Team Name"
+            floatingLabelText="Tea Name"
+            data-statekey="name"
+            onChange={this.inputChange}
+          />
+          <br />
+          <TextField
+            hintText="City"
+            floatingLabelText="City"
+            data-statekey="city"
+            onChange={this.inputChange}
+          />
+          <br />
+          <TextField
+            hintText="Country"
+            floatingLabelText="Country"
+            data-statekey="country"
+            onChange={this.inputChange}
+          />
+          <br />
+          <RaisedButton label="Add" style={styles.button} type="submit"/>
+        </form>
       </div>
     )
   }
